27cf5b489f63463824924ffde5bc84c3ed2719ce
Bianca <biancaemily018@gmail.com>
2026-09-18 23:53:32 +0000

    Corrige a capitalização da palavra "status" no template HTML e ajusta a importação do Flask no app.py

---
+++ Python_projet/app.py
@@ -0,0 +1,19 @@
+from flask import flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    #dados que serão calculados pelo python
+    titulo_pagina = "Dashboard de Demonstração"
+    aluno = {"nome": "Lucas", "Curso": "Engenharia de Software", "nota": 9.5}
+    status = "Aprovado" if aluno ["nota"] >= 5.0 else "Reprovado"
+
+return render_template (
+"index.html",
+usuario = aluno,
+resultado = status
+)
+
+if __name__ == "__main__":
+    app.run(debug=True)
