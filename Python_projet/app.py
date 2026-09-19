cc7b781f31fd2aba763230a9ec038ba46489634a
Bianca <biancaemily018@gmail.com>
2026-09-19 00:10:53 +0000

    Formata o retorno da função `render_template` em app.py e adiciona arquivos de tipagem para Flask

--- Python_projet/app.py
+++ Python_projet/app.py
@@ -9,11 +9,11 @@ def home():
     aluno = {"nome": "Lucas", "Curso": "Engenharia de Software", "nota": 9.5}
     status = "Aprovado" if aluno ["nota"] >= 5.0 else "Reprovado"
 
-return render_template (
-"index.html",
-usuario = aluno,
-resultado = status
-)
+    return render_template(
+        "index.html",
+        usuario=aluno,
+        resultado=status,
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
